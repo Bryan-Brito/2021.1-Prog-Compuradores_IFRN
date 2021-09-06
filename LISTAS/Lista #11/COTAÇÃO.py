00425e6c0c3c9d90e8a2747e76c2620e7ea4228a
Bryan Eric Oliveira de Brito <bryan.brito@academico.ifrn.edu.br>
2021-09-05 22:23:04 -0300

    Delete LISTAS/Lista #11 directory

--- LISTAS/Lista #11/COTAÇÃO.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url_cotacao  = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/'
-url_cotacao += 'CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?'
-url_cotacao += '@dataInicial=\'01-01-2020\'&@dataFinalCotacao=\'12-31-2020\'&$format=json'
-
-dict_cotacao = requests.get(url_cotacao, verify=True).json()
-
-print(dict_cotacao)
--- LISTAS/Lista #11/dicionario_cartola_fc.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-url_cartola = 'https://api.cartolafc.globo.com/atletas/mercado'
-
-dict_cartola = requests.get(url_cartola, verify=True).json()
